94e71c27cfcdb6425a62783a926a0763295114d0
selvakumari k <kselvkumari737@gmail.com>
2026-01-14 14:50:12 +0530

    day14 assignment added

---
+++ javascript/day14assignment/day14.js
@@ -0,0 +1,21 @@
+// first Question
+console.log("Hello, JavaScript!");
+
+// Second Question
+let num1 = 10;
+let num2 = 20;
+let sum = num1 + num2;
+console.log("Sum is:", sum);
+
+// Third Question
+let number = 7;
+if (number % 2 === 0) {
+    console.log(number + " is Even");
+} else {
+    console.log(number + " is Odd");
+}
+
+
+// Fourth Question
+let userName = prompt("Enter your name");
+console.log("Hello, " + userName + "!");
